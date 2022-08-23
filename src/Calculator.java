c1426daccda3b80648bafd780316020cf57487be
Kit Bolourchi <kitbolourchi@Kit-Bolourchis-MacBook-Air.local>
2022-08-23 14:54:36 +0100

    Added user input

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,32 +1,29 @@
-import java.util.Random;
 import java.util.Scanner;
 public class Calculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
 
         System.out.println("Welcome to the calculator!");
         System.out.println("================================");
 
-        int randomNumber1 = random.nextInt(100);
-        int randomNumber2 = random.nextInt(100);
-
-        System.out.println("Number 1: " + randomNumber1);
-        System.out.println("Number 2: " + randomNumber2);
+        System.out.print("Select your first number: ");
+        int number1 = Integer.parseInt(scanner.nextLine());
+        System.out.print("Select your second number: ");
+        int number2 = Integer.parseInt(scanner.nextLine());
 
         System.out.println("Select '+' (ADD), '*'(MULTIPLY), '-' (SUBTRACT)");
         String operation = scanner.nextLine();
         System.out.println("================================");
 
-        System.out.print(randomNumber1 + " " + operation + " " + randomNumber2 + " = ");
+        System.out.print(number1 + " " + operation + " " + number2 + " = ");
 
         if (operation.equals("+")) {
-            System.out.print(randomNumber1 + randomNumber2);
+            System.out.print(number1 + number2);
         } else if (operation.equals("*")) {
-            System.out.print(randomNumber1 * randomNumber2);
+            System.out.print(number1 * number2);
         } else {
-            System.out.print(randomNumber1 - randomNumber2);
+            System.out.print(number1 - number2);
         }
     }
 }
